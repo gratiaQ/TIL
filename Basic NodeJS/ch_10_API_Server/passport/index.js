bfec8b13fd04acce83eb83994c3abab3191f15a6
cafe43 <cafe43@naver.com>
2023-02-06 03:00:11 +0900

    230203] NodeJS API Server-1

--- Basic NodeJS/ch_10_API_Server/passport/index.js
+++ Basic NodeJS/ch_10_API_Server/passport/index.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const local = require('./localStrategy');
 const kakao = require('./kakaoStrategy');
-const User = require('../models/user');
+const { User } = require('../models');
 
 module.exports = () => {
   passport.serializeUser((user, done) => {
@@ -9,7 +9,54 @@ module.exports = () => {
   });
 
   passport.deserializeUser((id, done) => {
-    User.findOne({ where: { id } })
+    User.findOne({
+      where: { id },
+      include: [
+        {
+          model: User,
+          attributes: ['id', 'nick'],
+          as: 'Followers',
+        },
+        {
+          model: User,
+          attributes: ['id', 'nick'],
+          as: 'Followings',
+        },
+      ],
+    })
+      .then((user) => done(null, user))
+      .catch((err) => done(err));
+  });
+
+  local();
+  kakao();
+};
+const passport = require('passport');
+const local = require('./localStrategy');
+const kakao = require('./kakaoStrategy');
+const { User } = require('../models');
+
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findOne({
+      where: { id },
+      include: [
+        {
+          model: User,
+          attributes: ['id', 'nick'],
+          as: 'Followers',
+        },
+        {
+          model: User,
+          attributes: ['id', 'nick'],
+          as: 'Followings',
+        },
+      ],
+    })
       .then((user) => done(null, user))
       .catch((err) => done(err));
   });
